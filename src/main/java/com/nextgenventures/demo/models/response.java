736b5c09cfb08a880526718e711e15bf714deaab
yashlanjewar597 <97786189+yashlanjewar597@users.noreply.github.com>
2023-10-08 23:38:53 +0530

    Update response.java

--- src/main/java/com/nextgenventures/demo/models/response.java
+++ src/main/java/com/nextgenventures/demo/models/response.java
@@ -18,14 +18,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class response {
     
-    private ObjectId restaurant_id;
+    //private ObjectId restaurant_id;
     private String restaurantName;
     private double restaurantAvgRating;
-    private List<review> restaurantReviews;
+    //private List<review> restaurantReviews;
     private String dishName;
-    private String dishPrice;
-    private List<review> dishReviews;
-    private String dish_image_link;
+    //private String dishPrice;
+    //private List<review> dishReviews;
+    //private String dish_image_link;
     
 
 
