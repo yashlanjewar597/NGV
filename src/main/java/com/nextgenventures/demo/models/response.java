39137a89499625793a890018992dfe0f4ee56fce
yashlanjewar597 <97786189+yashlanjewar597@users.noreply.github.com>
2023-10-14 11:32:51 +0530

    Update response.java

--- src/main/java/com/nextgenventures/demo/models/response.java
+++ src/main/java/com/nextgenventures/demo/models/response.java
@@ -20,12 +20,12 @@ public class response {
     
     private ObjectId restaurant_id;
     private String restaurantName;
-    //private double restaurantAvgRating;
+    private double restaurantAvgRating;
     //private List<review> restaurantReviews;
     private String dishName;
-    //private String dishPrice;
-    //private List<review> dishReviews;
-    //private String dish_image_link;
+    private String dishPrice;
+    private List<review> dishReviews;
+    private String dish_image_link;
     
 
 
